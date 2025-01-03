3e72a93d91dc040151a07ae7e9227264897f1225
Jahirul Islam <jahirulislamjantu002@gmail.com>
2025-01-03 16:06:09 +0600

    tasbih

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -9,19 +9,36 @@ const CounterApp = () => {
      }
 
     return (
-        <div>
+        <div className="flex justify-center align-center mb-20">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="flex justify-center flex-col align-items-center w-auto gap-3 p-4 ">
-                                    <h1 className="text-center text-pink-400 text-6xl"> {count}  </h1>
-                                    <button onClick={IncrementCounter} className="btn btn-outline btn-accent">Increment</button>
-                                    <button onClick={DecrementCounter} className="btn btn-outline btn-accent">Decrement</button>
-
+                    <div className="card glass m-20">
+                        <figure>
+                            <img
+                                src="tasbihBG.jpeg"
+                                alt="car!"/>
+                        </figure>
+                        <div className="card-body bg-amber-50">
+                            <h2 className="card-title text-center text-pink-400 text-3xl card-title">Let's Count {count}</h2>
+                            <p className="text-pink-800">You Can count your Tasbih here!</p>
+                            <div className="card-actions justify-content-center align-items-center">
+                                <button onClick={IncrementCounter} className="btn btn-outline btn-accent ">Increment
+                                </button>
+                                <button onClick={DecrementCounter} className="btn btn-outline btn-accent ">Decrement
+                                </button>
                             </div>
                         </div>
                     </div>
+                    {/*<div className="card">*/}
+                    {/*    <div className="card-body">*/}
+                    {/*        <h1 className="text-center text-pink-400 text-6xl card-title"> {count}  </h1>*/}
+                    {/*        <button onClick={IncrementCounter} className="btn btn-outline btn-accent ">Increment*/}
+                    {/*        </button>*/}
+                    {/*        <button onClick={DecrementCounter} className="btn btn-outline btn-accent ">Decrement*/}
+                    {/*        </button>*/}
+
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
